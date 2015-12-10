b08d7acbfe1d7160420ffbf36b47b9326f7b6bcd
tholuck20 <maxandre.neri@gmail.com>
2015-12-10 18:09:12 +0100

    Update Black_Scholes.cpp

--- Black_Scholes/Black_Scholes.cpp
+++ Black_Scholes/Black_Scholes.cpp
@@ -25,6 +25,11 @@
 
 using namespace std;
 
+double f(double x){
+    return exp(-0.5 * x * x) / sqrt(2 * PI);
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // N1 - First method: an approximation of the cumulative distribution function (CDF)
 double N1(double x){
     double sum = x;
@@ -36,7 +41,9 @@ double N1(double x){
     }
     return 0.5 + (sum / sqrt(2*PI)) * exp(-(x * x)/2);
 };
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // N2 - Second method
 double N2(double x){
     double Q = 1 / (1 + 0.2316419 * abs(x));
@@ -49,14 +56,10 @@ double N2(double x){
     else
         return 1-temp;
 };
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// N3 - Third method
-double N3(double x){
-    return 1 / sqrt(2 * PI) * exp(-0.5 * x * x);
-};
-
-// N4 - Fourth method (not from me and in 2 parts)
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// N3 - Third method (not from me and in 2 parts)
     // Boole's Rule
     double Boole(double StartPoint, double EndPoint, int n) {
         vector<double> X(n+1, 0.0);
@@ -75,15 +78,15 @@ double N3(double x){
     }
 
     // N(0,1) cdf by Boole's Rule
-    double N4(double x) {
+    double N3(double x) {
         return Boole(-10.0, x, 240);
     };
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 // Black and Scholes Price
 double BSPrice(double S, double K, double r, double T, double v, char optType){
-    double d1 = (log(S / K) + (r + (v * v) / 2) * T) / (v * sqrt(T));
+    double d1 = (log(S / K) + (r + 0.5 * v * v) * T) / (v * sqrt(T));
     double d2 = d1 - v * sqrt(T);
 
     if (optType == 'C')
@@ -102,33 +105,14 @@ double BSDelta(double S, double K, double r, double T, double v, char optType, d
 };
 
 // Black and Scholes Gamma
-double BDGamma(double S, double K, double r, double T, double v){
+double BDGamma(double S, double K, double r, double T, double v, double q = 0){
+    double d1 = (log(S / K) + (r - q + 0.5* v * v) * T) / (v * sqrt(T));
 
+    return f(d1) / (S * v * sqrt(T));
 };
 
 
-// Black-Scholes Gamma
-double BSGamma(double S, double K, double T, double r, double v)
-{
-    double d = (log(S/K) + T*(r + 0.5*v*v)) / (v*sqrt(T));
-    return f(d) / S / v / sqrt(T);
-}
 
-double f(double x) {
-    double pi =  4.0*atan(1.0);
-    return exp(-x*x*0.5)/sqrt(2*pi);
-}
-
-/*
-Call B&S Price = 1.7691700522
-Put  B&S Price = 5.0621999098
-
-d1 B&S = -0.29160272
-d2 B&S = -0.54160272
-
-Delta B&S = 0.3852951980
-Delta B&S = -0.6147048020
-*/
 
 int main()
 {
