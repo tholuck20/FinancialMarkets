13478f0fd809ef771cf97ba52d1b5f96dade91e6
tholuck20 <maxandre.neri@gmail.com>
2015-12-10 18:09:24 +0100

    Update Black_Scholes.h

--- Black_Scholes/Black_Scholes.h
+++ Black_Scholes/Black_Scholes.h
@@ -10,15 +10,29 @@ using namespace std;
 
 
 // Tools
+/* As N is a density (integral), we can use several method to approximate the function:
+ *
+ - Méthode des rectangles / Rectangle method
+ - Méthode des trapèzes / Trapezoidal rule
+ - Méthode de Simpson / Simpson's rule
+ - Méthode de Gauss-Legendre /
+ - Midpoint method
+ - Heun's method
+ - Euler method
+ - Verlet integration
+ *
+ Which one is the most appropriate to approximate the Standard normal Probability Density Function?
+ *
+ Use PDF or CDF in Black and Scholes valuation?
+ *
+*/
 
 double N1(double x); // Standard normal Cumulative distribution function (CDF) = Fonction de répartition
 double N2(double x);
-
-double N3(double x); // Standard normal Probability Density Function (PDF) // N1 = N3 at 4 numbers after decimal point
-
-double N4(double x); // N1 = N4 at 9 numbers after the decimal point
+double N3(double x); // N1 = 34 at 9 numbers after the decimal point
 double Boole(double StartPoint, double EndPoint, int n);
 
+double f(double x); // Standard normal Probability Density Function (PDF) = Densité de probabilité
 
 // Black and Scholes valuation
 double BSPrice(double S, double K, double r, double T, double v, char optType);
