4e08d96b78dfd7e56382f43ecec3b47e6555ad8c
tholuck20 <maxandre.neri@gmail.com>
2015-12-11 10:41:13 +0100

    Update Bond_Options.cpp

--- Derivatives/Bond_Options/Bond_Options.cpp
+++ Derivatives/Bond_Options/Bond_Options.cpp
@@ -1,5 +1,10 @@
 // BlackBondOpt(BondLife,Coupon,Princ,Frequ,K,T,vol,IsCall,IsQuoted,Zeros,Result)
+// Calculations for bond options usng Black's model
 
 // HullWhiteBondOpt(BondLife,Coupon,Princ,Frequ,K,T,sigma,a, IsCall,IsQuoted,Zeros,Result)
+// Calculations for bond options using Hull-White model
 
 // TreeBondOpt(BondLife,Coupon,Princ,Frequ,K,T,sigma,a, IsCall,IsQuoted,IsAmerican,Model,nSteps,Zeros,Result)
+// Calculations for bond options usng a trinomial tree
+
+//BondPricing(BondLife, Coupon, Princ, Frequ, Zeros, IsClean, Result)
