1290fa395e6cb4f56e81ed1266256d343cd9c046
tholuck20 <maxandre.neri@gmail.com>
2015-12-11 10:47:12 +0100

    Create Exotic_Options.h

---
+++ Derivatives/Exotic_Options/Exotic_Options.h
@@ -0,0 +1,32 @@
+//
+// Created by mneri092115 on 10/12/2015.
+//
+
+#ifndef DERIVATIVES_EXOTIC_OPTIONS_H
+#define DERIVATIVES_EXOTIC_OPTIONS_H
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+
+// BinaryOption(S, K, r, q, vol, T, IsCall, IsFut, Divs, IsCash, Result)
+// Calculations for binary options on stocks, stock indices,currencies and futures
+
+// BarrierOption(S, K, r, q, vol, T, IsCall, IsFut, H, IsUp, IsIn, Result)
+// Calculations for barrier options on non-dividend-paying stocks, stock indices, currencies and futures
+
+// AverageOption(S, K, r, q, vol, T, IsCall, IsFut, CurrAve, TimeSoFar, Result)
+// Calculations for Asian options on non-dividend-paying stocks, stock indices, currencies and futures
+
+// ChooserOption(S, K, r, q, vol, T, IsFut, TimeToChoice, Result)
+// Calculations for chooser options on non-dividend-paying stocks, stock indices,currencies and futures
+
+// CompoundOption(S, K1, r, q, vol, T1, IsCall, IsFut, K2, T2, IsOptionOnCall, Result)
+// Calculations for compound options on non-dividend-paying stocks, stock indices,currencies and futures
+
+// LookbackOption(S, r, q, vol, T, IsCall, IsFut, IsFixedLookback, Smax, Smin, K, Result)
+// Calculations for lookback options on non-dividend-paying stocks, stock indices,currencies and futures
+
+
+#endif //DERIVATIVES_EXOTIC_OPTIONS_H
