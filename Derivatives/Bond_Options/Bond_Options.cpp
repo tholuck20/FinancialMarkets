bffd193aa2dab5fd2c92fd72f41d663a545b22ef
tholuck20 <maxandre.neri@gmail.com>
2015-12-11 10:36:33 +0100

    Create Bond_Options.cpp

---
+++ Derivatives/Bond_Options/Bond_Options.cpp
@@ -0,0 +1,5 @@
+// BlackBondOpt(BondLife,Coupon,Princ,Frequ,K,T,vol,IsCall,IsQuoted,Zeros,Result)
+
+// HullWhiteBondOpt(BondLife,Coupon,Princ,Frequ,K,T,sigma,a, IsCall,IsQuoted,Zeros,Result)
+
+// TreeBondOpt(BondLife,Coupon,Princ,Frequ,K,T,sigma,a, IsCall,IsQuoted,IsAmerican,Model,nSteps,Zeros,Result)
