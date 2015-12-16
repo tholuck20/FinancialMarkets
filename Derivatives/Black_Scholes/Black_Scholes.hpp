fc259e539bca903f5b578ae912daae6534ff7859
tholuck20 <maxandre.neri@gmail.com>
2015-12-16 14:35:04 +0100

    Update Black_Scholes.hpp

--- Derivatives/Black_Scholes/Black_Scholes.hpp
+++ Derivatives/Black_Scholes/Black_Scholes.hpp
@@ -2,12 +2,13 @@
 // Created by mneri092115 on 10/12/2015.
 //
 
-#ifndef DERIVATIVES_BLACK_SCHOLES_H
-#define DERIVATIVES_BLACK_SCHOLES_H
+
 #include <vector>
 #include <cmath>
 using namespace std;
 
+#ifndef DERIVATIVES_BLACK_SCHOLES_H
+#define DERIVATIVES_BLACK_SCHOLES_H
 
 // Tools
 /* As N is a density (integral), we can use several methods to approximate the function:
@@ -34,30 +35,37 @@ double Boole(double StartPoint, double EndPoint, int n);
 // Standard normal Probability Density Function (PDF) = Densité de probabilité
 double pdf(double x);
 
-// Black and Scholes valuation
-double BSPrice(double S, double K, double T, double r, double v, char optType, double q = 0);
-double BSImplVol(double S, double K, double T, double r, double v, double optType, double q = 0);
-double BSImplVol2(double S, double K, double T, double r, double v, double optType, double q = 0);
-double BSImplVol3(double S, double K, double T, double r, double v, double optType, double q = 0);
+class BS {
 
-// Black and Scholes Greeks
-double BSDelta(double S, double K, double T, double r, double v, char optType, double q = 0);
-double BSGamma(double S, double K, double T, double r, double v, double q = 0);
-double BSVega(double S, double K, double T, double r, double v, double q = 0);
-double BSRho(double S, double K, double T, double r, double v, char optType, double q = 0);
-double BSTheta(double S, double K, double T, double r, double v, char optType, double q = 0);
+public:
+    // Black and Scholes valuation
+    double BSPrice(double S, double K, double T, double r, double v, char optType, double q = 0);
 
-double BSVanna(double S, double K, double T, double r, double v, double q = 0, double b = 0); //////
-double BSCharm(double S, double K, double T, double r, double v, char optType, double q = 0, double b = 0); //////
+    // Black and Scholes implied volatility
+    double BSImplVol(double S, double K, double T, double r, double v, char optType, double q = 0);
+    double BSImplVol2(double S, double K, double T, double r, double v, char optType, double q = 0);
+    double BSImplVol3(double S, double K, double T, double r, double v, char optType, double q = 0);
+    double BSImplVol4(double S, double K, double T, double r, char optType, double b, double cm);
 
-double BSSpeed(double S, double K, double T, double r, double v, char optType);
-double BSZomma(double S, double K, double T, double r, double v, char optType);
-double BSColor(double S, double K, double T, double r, double v, char optType);
-double BSdVega_dTime(double S, double K, double T, double r, double v, char optType);
-double BSVomma(double S, double K, double T, double r, double v, char optType);
-double BSDualDelta(double S, double K, double T, double r, double v, char optType);
-double BSDualGamma(double S, double K, double T, double r, double v, char optType);
+    // Black and Scholes Greeks
+    double BSDelta(double S, double K, double T, double r, double v, char optType, double q = 0);
+    double BSGamma(double S, double K, double T, double r, double v, double q = 0);
+    double BSVega(double S, double K, double T, double r, double v, double q = 0);
+    double BSRho(double S, double K, double T, double r, double v, char optType, double q = 0);
+    double BSTheta(double S, double K, double T, double r, double v, char optType, double q = 0);
+    double BSVanna(double S, double K, double T, double r, double v, double q = 0, double b = 0); //////
+    double BSCharm(double S, double K, double T, double r, double v, char optType, double q = 0, double b = 0); //////
+    double BSSpeed(double S, double K, double T, double r, double v, char optType);
+    double BSZomma(double S, double K, double T, double r, double v, char optType);
+    double BSColor(double S, double K, double T, double r, double v, char optType);
+    double BSdVega_dTime(double S, double K, double T, double r, double v, char optType);
+    double BSVomma(double S, double K, double T, double r, double v, char optType);
+    double BSDualDelta(double S, double K, double T, double r, double v, char optType);
+    double BSDualGamma(double S, double K, double T, double r, double v, char optType);
 
+
+
+};
 
 
 
