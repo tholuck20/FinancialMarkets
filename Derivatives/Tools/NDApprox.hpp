0eda00decbd1d393696fd839514c2cc4c67f0022
tholuck20 <maxandre.neri@gmail.com>
2015-12-29 18:17:34 +0100

    Update NDApprox.hpp

--- Derivatives/Tools/NDApprox.hpp
+++ Derivatives/Tools/NDApprox.hpp
@@ -12,27 +12,69 @@ class NDApprox
 
 public:
     //distributions
-    double PDF(double x); // Probability Density Function (= Normal Distribution Function)
-    double CND(double x); // Marsaglia (2004)
-    double CND2(double x); // From volopta.com
-        double Boole(double StartPoint, double EndPoint, int n);
-    double CND3(double x); // Abramowitz and Stegun approximation
-    double CND4(double x); // Shore  (2005)
-    double CND5(double x); // Double precision univariate normal function
+    static double PDF(double x); // Probability Density Function (= Normal Distribution Function)
+    static double CND(double x); // Marsaglia (2004)
+    static double CND2(double x); // From volopta.com
+    static double Boole(double StartPoint, double EndPoint, int n);
+    static double CND3(double x); // Abramowitz and Stegun approximation
+    static double CND4(double x); // Shore  (2005)
+    static double CND5(double x); // Double precision univariate normal function
 
-    double BND(double X, double y, double rho); // Bivariate Normal Distribution
-    double CBND(double X, double y, double rho); // Cumulative Bivariate Normal Distribution
-    double CBND2(double A,double b, double rho);
-    double CBND3(double A, double b, double rho);
-    double CBND4(double A, double b, double rho);
-    double CBNDGeneral(int TypeFlag, double X, double y, double rho);
-    double CNDEV(double p);
+    static double BND(double X, double y, double rho); // Bivariate Normal Distribution
+    static double CBND(double X, double y, double rho); // Cumulative Bivariate Normal Distribution
+    static double CBND2(double A,double b, double rho);
+    static double CBND3(double A, double b, double rho);
+    static double CBND4(double A, double b, double rho);
+    static double CBNDGeneral(int TypeFlag, double X, double y, double rho);
+    static double CNDEV(double p);
 
 
+private:
 
+    // Boole's Rule
+    static double Boole(double StartPoint, double EndPoint, int n)
+    {
+        vector<double> X(n+1, 0.0);
+        vector<double> Y(n+1, 0.0);
+        double delta_x = (EndPoint - StartPoint)/double(n);
+        for (int i=0; i<=n; ++i) {
+            X[i] = StartPoint + i*delta_x;
+            Y[i] = NDApprox::PDF(X[i]);
+        }
+        double sum = 0;
+        for (int t=0; t<=(n-1)/4; ++t) {
+            int ind = 4*t;
+            sum += (1/45.0)*(14*Y[ind] + 64*Y[ind+1] + 24*Y[ind+2] + 64*Y[ind+3] + 14*Y[ind+4])*delta_x;
+        }
+        return sum;
+    }
+
+    static double Factorial(double n)
+    {
+        if(n<=0)return 1.0;
+        double res=1.0;
+        for (double i = 2.0; i < (n + 1); i++)
+        {
+            res *= i;
+        }
+        return res;
+    }
+
+    static double Combination(double n, double r) {
+        double nfloor = floor(n);
+        double rfloor = floor(r);
+
+        if ((n < 1) || (r < 0) || (r > n) || (r != rfloor) || (n != nfloor)) {
+            return 0.0;
+        }
+        return Factorial(n) / (Factorial(r) * Factorial(n - r));
+    }
+
+    static double Sign(double x)
+    {
+        return (x<0.0) ? -1.0 : 1.0;
+    }
 
 };
-
-
 
 #endif //C_NDAPPROX_H
