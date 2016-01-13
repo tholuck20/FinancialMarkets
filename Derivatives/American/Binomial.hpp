b855ca6c7673252ad4cd3887f183cca3f79191d2
tholuck20 <maxandre.neri@gmail.com>
2016-01-13 17:39:55 +0100

    Update Binomial.hpp

--- Derivatives/American/Binomial.hpp
+++ Derivatives/American/Binomial.hpp
@@ -5,30 +5,19 @@
 #ifndef C_AMERICAN_HPP
 #define C_AMERICAN_HPP
 
-
-
-
-
     // Barone-Adesi and Whaley Approximation
     double BAWApprox(double S, double K, double T, double r, double v, char optType, double b);
     // Newton-Raphson algorithm to solve critical commodity price
     double CCTYPrice(double K, double T, double r, double v, char optType, double b);
 
     // Binomial valuation for American options
-    double Binomial(double S, double K, double T, double r, double v, char optType, double q);
+    double Binomial(double S, double K, double T, double r, double v, char optType, double q, int n = 500);
 
     // Leisen Reimer Binomial Tree (optStyle = European/American)
     double LRBinomial(double S, double K, double T, double r, double v, char optType, char optStyle, int Method = 1);
-        // Method: Peizer-Pratt Inversion method (1 or 2)
-
-    // Extrapolated Flexible Binomial Tree
-    double FlexBinomial(double S, double K, double T, double r, double v, char optType, char optStyle); // n
-
-    // Flexible Binomial Tree from Tian (1991)
-    double TianFlexBinomial(double S, double K, double T, double r, double v, char optType, char optStyle); // n + lambda
-
-    // Cox Ross Rubinstein Binomial Tree
-    double CRRBinomial(double S, double K, double T, double r, double v, char optType, char optStyle); // int n
-
+        // Method: Peizer-Pratt Inversion method (1 or 2) (does not change anything)
+        // Results: OK for European Option (both Call & Put)
+        //          nOK for American Call Option (same value as European Call)
+        //          OK for American Put Option
 
 #endif //C_AMERICAN_HPP
