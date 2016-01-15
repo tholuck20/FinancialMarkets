15b0f52f26a5b3f67fce6bbbfd9ffaf4a85e4b45
tholuck20 <maxandre.neri@gmail.com>
2016-01-15 18:08:39 +0100

    Update BlackScholes.h

--- Test/BlackScholes.h
+++ Test/BlackScholes.h
@@ -1,1 +1,22 @@
 
+#pragma once
+
+#include <math.h>
+
+
+
+class CBlackScholes {
+
+public:
+
+        CBlackScholes(void);
+
+        ~CBlackScholes(void);
+
+        double normcdf(double z);
+
+        double BSPrice(double S,double K,double r,double q,double v,double T,char PutCall);
+
+};
+
+
