16e3b08f7d4a57cb0bf4813bfe1833483a44a727
tholuck20 <maxandre.neri@gmail.com>
2016-01-15 18:10:19 +0100

    Update BlackScholesLSM.h

--- Test/BlackScholesLSM.h
+++ Test/BlackScholesLSM.h
@@ -1,1 +1,26 @@
 
+#pragma once
+
+#include <algorithm>
+
+#include <set>
+
+#include <iterator>
+
+#include "MatrixOperations.h"
+
+
+
+class CBlackScholesLSM {
+
+public:
+
+        CBlackScholesLSM(void);
+
+        ~CBlackScholesLSM(void);
+
+        vector<double> LSM(vector<vector<double> > S,double K,double r,double q,double T,int NT,int NS,char PutCall);
+
+};
+
+
