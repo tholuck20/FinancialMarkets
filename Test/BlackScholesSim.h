96eccba22b20f1c732de471d20833b56a7c33bf2
tholuck20 <maxandre.neri@gmail.com>
2016-01-15 18:11:42 +0100

    Update BlackScholesSim.h

--- Test/BlackScholesSim.h
+++ Test/BlackScholesSim.h
@@ -1,1 +1,30 @@
 
+#pragma once
+
+
+
+#include <math.h>
+
+#include <vector>
+
+#include <time.h>
+
+using namespace std;
+
+
+
+
+
+class CBlackScholesSim {
+
+public:
+
+        CBlackScholesSim(void);
+
+        ~CBlackScholesSim(void);
+
+        vector<vector<double> > CBlackScholesSim::GenerateBSpaths(double S0,double T,double rf,double q,double v,int NS,int NT);
+
+};
+
+
