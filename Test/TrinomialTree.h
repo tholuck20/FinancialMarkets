ef9a0a8931da2409d95034ed5fdb829365e6ba95
tholuck20 <maxandre.neri@gmail.com>
2016-01-15 18:15:11 +0100

    Update TrinomialTree.h

--- Test/TrinomialTree.h
+++ Test/TrinomialTree.h
@@ -1,1 +1,22 @@
 
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+
+
+class CTrinomialTree {
+
+public:
+
+        CTrinomialTree(void);
+
+        ~CTrinomialTree(void);
+
+        double Trinomial(double Spot,double K,double r,double q,double v,double T,char PutCall,char EuroAmer,int n);
+
+};
+
+
