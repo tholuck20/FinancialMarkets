a41b9a749804e9a8b778aa583ac002abf03729b2
tholuck20 <maxandre.neri@gmail.com>
2016-01-28 14:19:53 +0100

    Update ExoticOptions.hpp

--- Derivatives/Exotic_Options/ExoticOptions.hpp
+++ Derivatives/Exotic_Options/ExoticOptions.hpp
@@ -4,14 +4,10 @@
 
 #ifndef EXOTIC_OPTIONS_H
 #define EXOTIC_OPTIONS_H
-#include <vector>
-#include <cmath>
-
-using namespace std;
 
 
-// BinaryOption(S, K, r, q, vol, T, IsCall, IsFut, Divs, IsCash, Result)
-// Calculations for binary options on stocks, stock indices,currencies and futures
+
+// Price and Greeks for Binary Options
 double BinaryOptions(double S, double K, double T, double r, double v, char optType, bool isCash,
                      bool isFut = false, double q = 0, int result = 0);
 double Binary(double S, double K, double T, double r, double v, char optType, bool isCash, double q = 0);
