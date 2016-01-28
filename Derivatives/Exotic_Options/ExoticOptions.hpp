c5586a7fbdd1a6cb289191b7a7b205a1a16eba04
tholuck20 <maxandre.neri@gmail.com>
2016-01-28 14:13:02 +0100

    Update and rename Exotic_Options.hpp to ExoticOptions.hpp

--- Derivatives/Exotic_Options/ExoticOptions.hpp
+++ Derivatives/Exotic_Options/ExoticOptions.hpp
@@ -12,6 +12,9 @@ using namespace std;
 
 // BinaryOption(S, K, r, q, vol, T, IsCall, IsFut, Divs, IsCash, Result)
 // Calculations for binary options on stocks, stock indices,currencies and futures
+double BinaryOptions(double S, double K, double T, double r, double v, char optType, bool isCash,
+                     bool isFut = false, double q = 0, int result = 0);
+double Binary(double S, double K, double T, double r, double v, char optType, bool isCash, double q = 0);
 
 // BarrierOption(S, K, r, q, vol, T, IsCall, IsFut, H, IsUp, IsIn, Result)
 // Calculations for barrier options on non-dividend-paying stocks, stock indices, currencies and futures
