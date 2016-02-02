c701efa985439f99a06bdd77afa8b008589e70b3
tholuck20 <maxandre.neri@gmail.com>
2016-02-02 10:36:49 +0100

    Update Monte_Carlo.hpp

--- Derivatives/Monte_Carlo/Monte_Carlo.hpp
+++ Derivatives/Monte_Carlo/Monte_Carlo.hpp
@@ -5,4 +5,14 @@
 #ifndef C_MONTECARLO_HPP
 #define C_MONTECARLO_HPP
 
+/* 1. Tirer au hasard une trajectoire de S dans l'univers risque-neutre
+ * 2. Calculer le flux payé à l'échéance par l'actif dérivé pour cette trajectoire
+ * 3. Répéter les étapes 1 et 2 de façon à disposer d'un grand nombre de flux terminaux
+      de l'actif dérivé dans l'univers risque-neutre
+ * 4. Calculer la moyenne des flux terminaux pour obtenir une estimation de l'espérance du flux terminal dans l'univers
+      risque-neutre
+ * 5. Actualiser le flux terminal espéré au taux d'intérêt sans risque pour obtenir une estimation de la valeur
+      de l'actif dérivé.
+*/
+
 #endif //C_MONTECARLO_HPP
