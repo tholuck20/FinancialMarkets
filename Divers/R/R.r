d52ff1a9ab786ad2ccbb5bfe0d1f9374a090605b
tholuck20 <maxandre.neri@gmail.com>
2016-02-05 15:29:02 +0100

    Update R.r

--- Divers/R/R.r
+++ Divers/R/R.r
@@ -1,3 +1,17 @@
+
+Tester le type d'une variable:
 is.character(variable)
 is.numeric(variable)
 is.logical(variable)
+
+Déclarer / créer des variables typées:
+as.numeric(42)
+as.numeric("42")
+as.character("Hello")
+as.character(42)
+
+Anciennes version de R (similaires à la fonction as.numeric()):
+  as.double()
+  as.single()
+  as.real()
+
