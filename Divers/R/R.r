3382048cfdecef406f17f1d09c5cf30bcfc72c15
tholuck20 <maxandre.neri@gmail.com>
2016-02-05 18:10:55 +0100

    Update R.r

--- Divers/R/R.r
+++ Divers/R/R.r
@@ -24,4 +24,15 @@ ceiling(2.4) donne 3 #Renvoie à l\'entier supérieur
 round(2.4) donne 2 #Renvoie à l\'entier le plus proche
 
 scan() #Lis les données entrées par l\'utilisateur (pour sortir taper sur Entrée sans mettre de valeur)
-paste("abc", scan(nmax=1), "def")
+#Manipuler chaîne de caractères:
+paste("abc", scan(nmax=1), "def") #Concaténation
+nchar() #Compte le nombre de caractère (espace compris)
+toupper() #Converti tout en MAJUSCULE
+tolower() #Converti tout en minuscule
+substr(x, start, strop) #Extraire une sous chaine allant du caractère n° start au caractère n° stop à partir de la chaine x
+
+ls() #Permet de lister toutes les variables créées
+ls(pattern="var") #Liste toutes les variables dont le nom contient "var"
+rm(var1) #Permet de supprimer la variable "var1"
+
+
