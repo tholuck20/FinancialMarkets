c81e52ec95c2b1e34c38219f40b01c786775bdbf
tholuck20 <maxandre.neri@gmail.com>
2016-02-05 16:19:59 +0100

    Update R.r

--- Divers/R/R.r
+++ Divers/R/R.r
@@ -9,6 +9,10 @@ as.numeric(42)
 as.numeric("42")
 as.character("Hello")
 as.character(42)
+as.logical() # 0 = False, 'Tout nombre != 0' = True ; "F" ou "FALSE" = FALSE, "T" ou "TRUE" = TRUE, 'Autre' = NA
+
+# NA : utilisé lorsque la valeur n\'existe pas ou n\'est pas définie
+# NULL : désigne une valeur nulle, indique le résultat n\'a pas de valeur (équivalent du 0 appliqué à tout type de données)
 
 Anciennes version de R (similaires à la fonction as.numeric()):
   as.double()
