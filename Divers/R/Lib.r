599f813870e6e9a0d9ce8d8822d3f1ff6eeaadbd
tholuck20 <maxandre.neri@gmail.com>
2016-05-27 18:27:28 +0200

    Update Lib.r

--- Divers/R/Lib.r
+++ Divers/R/Lib.r
@@ -2,8 +2,20 @@ library(Rcpp)
 library(RQuantLib) #isBusinessDay
 library(sfsmisc) #nearcor
 library(expm)
-library(polynom) 
+library(polynom)
+library(Matrix)
 
 
+
+source(paste(Script_Path,"Structure.r",sep=""))
 source(paste(Script_Path,"Tools.r",sep=""))
+source(paste(Script_Path,"Extract.r",sep=""))
+source(paste(Script_Path,"Clean.r",sep=""))
+source(paste(Script_Path,"Export.r",sep=""))
+source(paste(Script_Path,"Calibration.r",sep=""))
+
+
+
+
+
 
