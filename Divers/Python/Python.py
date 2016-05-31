6e0520bfb4c5903b270c468e1e3186f70d4008f7
tholuck20 <maxandre.neri@gmail.com>
2016-05-31 17:41:43 +0200

    Update Python.py

--- Divers/Python/Python.py
+++ Divers/Python/Python.py
@@ -9,4 +9,11 @@ myVar.isalpha() # alphabétiques
 myVar.isdigit() # numériques
 myVar.isspace() # espaces
 
+myVar.startswith(prefix[, start[, stop]]) # commence par...
+myVar.endswith(suffix[, start[, stop]]) # se termine par...
 
+# Suppriment toutes les combinaisons de chars (ou l'espace par défaut) 
+# respectivement au début et en fin, au début, ou en fin d'une chaîne :
+myVar.strip([chars])
+myVar.lstrip([chars])
+myVar.rstrip([chars])
