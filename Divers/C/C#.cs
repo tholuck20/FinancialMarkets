9afba36a888a3cc3e8641de4e4b11bfbd4b9366d
tholuck20 <maxandre.neri@gmail.com>
2016-06-15 11:33:46 +0200

    Update C#.cs

--- Divers/C/C#.cs
+++ Divers/C/C#.cs
@@ -1,3 +1,6 @@
 // Création de tableau 
 
+double[] D = new double[28]; // tableau fixe à 1 dimension
+double[,] P = new double[28, 28]; // tableau fixe à 2 dimensions
+
 List<double> tabl = new List<double>(); // tableau dynamique à 2 dimensions
